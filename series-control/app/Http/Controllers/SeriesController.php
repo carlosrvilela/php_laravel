8f425fd28b743531f1d443d5ecbd4cedae9099f1
carlosrvilela <carlosjr@ciandt.com>
2022-08-10 14:25:54 -0300

    added serie basic  model

--- series-control/app/Http/Controllers/SeriesController.php
+++ series-control/app/Http/Controllers/SeriesController.php
@@ -2,17 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Serie;
 use Illuminate\Http\Request;
 
 class SeriesController extends Controller
 {
     public function index()
     {
-        $series = [
-            'Serie A',
-            'Serie B',
-            'Serie C',
-        ];
+        $series = Serie::query()->orderBy('nome')->get();
 
         return view('series.index')->with('series', $series);
     }
@@ -20,5 +17,15 @@ class SeriesController extends Controller
     public function create()
     {
         return view('series.create');
+    }
+
+    public function store(Request $request)
+    {
+        $nomeSerie = $request->input('nome');
+        $serie = new Serie();
+        $serie->nome = $nomeSerie;
+        $serie->save();
+
+        return redirect('/series');
     }
 }
--- series-control/resources/views/series/create.blade.php
+++ series-control/resources/views/series/create.blade.php
@@ -1,9 +1,10 @@
 <x-layout title="Nova Série">
-    <form action="", method="post">
+    <form action="/series/salvar", method="post">
+        @csrf
         <div class="form-group mb-2">
             <label for="nome" class="form-label">Nome:</label>
             <input type="text" name="nome" id="nome" placeholder="nova série" class="form-control">
         </div>
-        <button type="submit" class="btn btn-dark">Adicinar</button>
+        <button type="submit" class="btn btn-dark">Adicionar</button>
     </form>
 </x-layout>
