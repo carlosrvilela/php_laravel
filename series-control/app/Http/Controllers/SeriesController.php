28092c6d645131515e76fd1411f5cbcf864ea435
carlosrvilela <carlosjr@ciandt.com>
2022-09-22 17:10:53 -0300

    added method to persist series->season->episode

--- series-control/app/Http/Controllers/SeriesController.php
+++ series-control/app/Http/Controllers/SeriesController.php
@@ -2,16 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\SeriesFormRequest;
-use App\Models\Serie;
+use App\Models\Season;
+use App\Models\Series;
+use App\Models\Episode;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Redis;
+use App\Http\Requests\SeriesFormRequest;
 
 class SeriesController extends Controller
 {
     public function index(Request $request)
     {
-        $series = Serie::all();
+        $series = Series::all();
         $successMessage = $request->session()->get('success.message');
 
         return view('series.index')
@@ -26,13 +27,33 @@ class SeriesController extends Controller
 
     public function store(SeriesFormRequest $request)
     {
-        $serie = Serie::create($request->all());
+        $serie = Series::create($request->all());
+
+        $seasons = [];
+        for ($seasonNumber = 1; $seasonNumber <= $request->seasonsQuantity; $seasonNumber++) {
+            $seasons [] = [
+                'series_id' => $serie->id,
+                'number' => $seasonNumber
+            ];
+        }
+        Season::insert($seasons);
+
+        $episodes = [];
+        foreach ($serie->seasons as $season) {
+            for ($episodeNumber = 1; $episodeNumber <= $request->episodesPerSeason; $episodeNumber++) {
+                $episodes [] = [
+                    'season_id' => $season->id,
+                    'number' => $episodeNumber
+                ];
+            }
+        }
+        Episode::insert($episodes);
 
         return to_route('series.index')
             ->with('success.message', "Série '{$serie->nome}' adicionada com sucesso");
     }
 
-    public function destroy(Serie $series)
+    public function destroy(Series $series)
     {
         $series->delete();
 
@@ -40,14 +61,13 @@ class SeriesController extends Controller
             ->with('success.message', "Série '{$series->nome}' removida com sucesso");
     }
 
-    public function edit(Serie $series, Request $request)
+    public function edit(Series $series, Request $request)
     {
-        dd($series->seasons);
         return view('series.edit')
             ->with('serie', $series);
     }
 
-    public function update(Serie $series, SeriesFormRequest $request)
+    public function update(Series $series, SeriesFormRequest $request)
     {
         $series->fill($request->all());
         $series->save();
--- series-control/resources/views/series/create.blade.php
+++ series-control/resources/views/series/create.blade.php
@@ -1,4 +1,42 @@
 <x-layout title="Nova Série">
-    <x-series.form :action="route('series.store')" :nome="old('nome')" :update="false" btnSubmitText="Adicionar">
-    </x-series.form>
+    <form action="{{ route('series.store') }}" method="post">
+        @csrf
+
+        <div class="row mb-3">
+
+            <div class="col-8">
+                <label for="nome" class="form-label">Nome:</label>
+                <input class="form-control"
+                    type="text"
+                    autofocus
+                    name="nome"
+                    id="nome"
+                    placeholder="nova série"
+                    value="{{ old('nome') }}">
+            </div>
+
+            <div class="col-2">
+                <label for="seasonsQuantity" class="form-label">N° Temporadas:</label>
+                <input class="form-control"
+                    type="text"
+                    name="seasonsQuantity"
+                    id="seasonsQuantity"
+                    placeholder="01"
+                    value="{{ old('seasonsQuantity') }}">
+            </div>
+
+            <div class="col-2">
+                <label for="episodesPerSeason" class="form-label">Episódeos por Temporada:</label>
+                <input class="form-control"
+                    type="text"
+                    name="episodesPerSeason"
+                    id="episodesPerSeason"
+                    placeholder="01"
+                    value="{{ old('episodesPerSeason') }}">
+            </div>
+
+        </div>
+
+        <button type="submit" class="btn btn-dark">Adicionar</button>
+    </form>
 </x-layout>
